ee87a97bd12fb5ff66da6ee6f561bef0532aca4f
Rynnavinx <35942714+Rynnavinx@users.noreply.github.com>
2023-08-05 21:46:37 -0700

    small optimization for dirt path model wrapping by removing the check on every loop

--- src/main/java/rynnavinx/sspb/mixin/minecraft/MixinModelLoader.java
+++ src/main/java/rynnavinx/sspb/mixin/minecraft/MixinModelLoader.java
@@ -3,14 +3,20 @@ package rynnavinx.sspb.mixin.minecraft;
 import net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.render.model.SpriteAtlasManager;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 
+import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import rynnavinx.sspb.client.SSPBClientMod;
 import rynnavinx.sspb.client.gui.SSPBGameOptionPages;
 import rynnavinx.sspb.client.render.model.SSPBBakedModel;
@@ -27,13 +33,15 @@ public class MixinModelLoader {
     @Final @Shadow
     private Map<Identifier, BakedModel> bakedModels;
 
+
     @Unique
-    private void wrapDirtPathModel(Identifier id){
-        if(id.getNamespace().equals("minecraft") && id.getPath().equals("dirt_path") && ((ModelIdentifier)id).getVariant().equals("")) {
-            BakedModel originalBakedModel = bakedModels.get(id);
+    private void wrapDirtPathModel(){
+        ModelIdentifier id = new ModelIdentifier("minecraft", "dirt_path", "");
+        BakedModel originalBakedModel = bakedModels.get(id);
+        if(originalBakedModel != null){
             // wrap if not using frapi
             if(!(originalBakedModel instanceof FabricBakedModel) || ((FabricBakedModel) originalBakedModel).isVanillaAdapter()){
-                bakedModels.replace(id, new SSPBBakedModel(bakedModels.get(id), SSPBClientMod.options().vanillaPathBlockLighting));
+                bakedModels.replace(id, new SSPBBakedModel(originalBakedModel, SSPBClientMod.options().vanillaPathBlockLighting));
                 SSPBGameOptionPages.setVanillaPathBlockLightingOptEnabled(true);
                 LOGGER.info("[SSPB] Option to toggle vanilla path block lighting is enabled");
             }
@@ -42,24 +50,25 @@ public class MixinModelLoader {
                 LOGGER.info("[SSPB] Modded dirt path rendering detected. Option to toggle vanilla path block lighting is disabled.");
             }
         }
+        else{
+            LOGGER.warn("[SSPB] Dirt Path model was not found");
+        }
     }
 
+    /* These two mixins target the same bit of code that got refactored in 1.19.3,
+     * so I'm injecting like this for compatibility reasons.
+     */
 
-   /* These two mixins target the same lambda, but the lambda changed position in the code from 1.19 to 1.20,
-    * which changed the intermediate mapping name, so I'm injecting like this for compatibility reasons.
-    */
-
-    //Targets lambda "method_4733" at the bottom of method "upload" in 1.19
-    @Dynamic()
-    @Inject(method = "method_4733", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", shift = At.Shift.AFTER), require = 0)
-    private void wrapDirtPathModel1_19(Identifier id, CallbackInfo ci) {
-        wrapDirtPathModel(id);
+    @Dynamic
+    @Inject(method = "upload", at = @At(value = "INVOKE", target = "Ljava/util/Set;forEach(Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER), require = 0)
+    private void wrapDirtPathModelPre1_19_3(TextureManager textureManager, Profiler profiler, CallbackInfoReturnable<SpriteAtlasManager> cir){
+        wrapDirtPathModel();
     }
 
-    //Targets lambda "method_45877" in the method "bake" in 1.20
-    @Dynamic()
-    @Inject(method = "method_45877", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", shift = At.Shift.AFTER), require = 0)
-    private void wrapDirtPathModel1_20(BiFunction biFunction, Identifier modelId, CallbackInfo ci) {
-        wrapDirtPathModel(modelId);
+    // Using intermediary name because the yarn mapped name "bake" is a separate method pre-1.19.3, which led to issues injecting
+    @Dynamic
+    @Inject(method = "method_45876(Ljava/util/function/BiFunction;)V", at = @At(value = "INVOKE", target = "Ljava/util/Set;forEach(Ljava/util/function/Consumer;)V", shift = At.Shift.AFTER), require = 0)
+    private void wrapDirtPathModel1_19_3(BiFunction<Identifier, SpriteIdentifier, Sprite> spriteLoader, CallbackInfo ci){
+        wrapDirtPathModel();
     }
 }
