0e8ffcefc3df1a0b9635e3333a8d0ba2bc0430b9
Rynnavinx <35942714+Rynnavinx@users.noreply.github.com>
2025-12-11 19:36:39 -0800

    Reimplement settings with the Sodium config API
    (cherry picked from commit c10b894f717fa9854bb964143d6765425f57be82)

--- common/src/main/java/rynnavinx/sspb/common/client/SSPBClientMod.java
+++ common/src/main/java/rynnavinx/sspb/common/client/SSPBClientMod.java
@@ -3,24 +3,21 @@ package rynnavinx.sspb.common.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import rynnavinx.sspb.common.client.gui.SSPBGameOptions;
+import rynnavinx.sspb.common.client.options.SSPBOptions;
 
 
 public class SSPBClientMod {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("SSPB");
 
-	private static SSPBGameOptions CONFIG;
+	private static final SSPBOptions CONFIG = SSPBOptions.load();
 
 
-	public static SSPBGameOptions options() {
+	public static SSPBOptions options() {
 		return CONFIG;
 	}
 
 	public static void onInitClient() {
-		CONFIG = SSPBGameOptions.load();
-		CONFIG.updateShadowyness(CONFIG.shadowynessPercent);
-
 		LOGGER.info("[SSPB] Broken dirt path lighting is best dirt path lighting lol");
 	}
 }
